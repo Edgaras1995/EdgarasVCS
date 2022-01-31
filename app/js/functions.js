6b70329823258b2e3f469521a614ebf5fd9e4350
Edgaras <95968686+Edgaras1995@users.noreply.github.com>
2022-01-31 16:33:11 +0200

    Add files via upload

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,5 +1,4 @@
-
-
+//MAIN Banner Slide Show
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -7,27 +6,19 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("banner-slides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" activedot", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " activedot";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000);
 }
 
+//Mobile Menu Funkcija
 function mobileMenu() {
   var x = document.getElementById("mMenu");
   if (x.style.display === "block") {
@@ -37,8 +28,3 @@ function mobileMenu() {
       x.style.display = "block";
   } 
 }
-
-$(document).ready(function(){
-  $('#contant').animate({
-      scrollTop: $('#contant')[0].scrollHeight}, 2000);
-});
